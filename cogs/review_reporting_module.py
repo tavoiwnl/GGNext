94f5ee9cf45360a0220269a11c8a1682090346f7
tavoiwnl <steickem@gmail.com>
2025-06-05 23:45:50 +0800

    Update review_reporting_module.py

--- cogs/review_reporting_module.py
+++ cogs/review_reporting_module.py
@@ -46,5 +46,6 @@ class ReviewReporting(commands.Cog):
             )
         await interaction.response.send_message(embed=embed, ephemeral=True)
 
-async def setup(bot, data_store):
-    await bot.add_cog(ReviewReporting(bot, data_store))
+async def setup(bot, extras):
+    data_store = extras["data_store"]
+    await bot.add_cog(YourCogClass(bot, data_store))

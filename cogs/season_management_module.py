67be481ab82933bfe8344cd79c8626ec14d25e8c
tavoiwnl <steickem@gmail.com>
2025-06-05 23:46:21 +0800

    Update season_management_module.py

--- cogs/season_management_module.py
+++ cogs/season_management_module.py
@@ -45,5 +45,7 @@ class SeasonManagement(commands.Cog):
 
         await interaction.response.send_message(embed=embed)
 
-async def setup(bot, data_store):
-    await bot.add_cog(SeasonManagement(bot, data_store))
+async def setup(bot, extras):
+    data_store = extras["data_store"]
+    await bot.add_cog(YourCogClass(bot, data_store))
+

f56b402b83b1f62712a824412755d9df5bc931b1
tavoiwnl <steickem@gmail.com>
2025-06-06 00:19:43 +0800

    Update season_leaderboard_module.py

--- cogs/season_leaderboard_module.py
+++ cogs/season_leaderboard_module.py
@@ -69,6 +69,6 @@ class SeasonalLeaderboards(commands.Cog):
         await interaction.response.send_message(embed=embed)
 
 # season_leaderboard_module.py
-async def setup(bot, extras):
-    data_store = extras["data_store"]
+async def setup(bot, data_store):
     await bot.add_cog(SeasonLeaderboard(bot, data_store))
+
